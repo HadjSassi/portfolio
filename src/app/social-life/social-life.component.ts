24228c84e723d509b6175a11fb238966bc67b76d
HadjSassi <mahdihadjsassi1@gmail.com>
2025-02-17 23:45:34 +0100

    Social Life

--- src/app/social-life/social-life.component.ts
+++ src/app/social-life/social-life.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {CommonModule} from '@angular/common';
+
+interface SocialActivity {
+  title: string;
+  image: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-social-life',
-  imports: [],
   templateUrl: './social-life.component.html',
-  styleUrl: './social-life.component.css'
+  styleUrls: ['./social-life.component.css'],
+  imports: [CommonModule]
 })
-export class SocialLifeComponent {
+export class SocialLifeComponent implements OnInit {
+  socialActivities: SocialActivity[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchSocialActivities();
+  }
+
+  fetchSocialActivities() {
+    this.http.get<{ socialActivities: SocialActivity[] }>('data/social.json').subscribe(data => {
+      this.socialActivities = data.socialActivities;
+    });
+  }
+
+  openLink(link: string) {
+    window.open(link, '_blank');
+  }
 }
