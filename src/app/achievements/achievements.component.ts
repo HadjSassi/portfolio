67ba6f8212612a0787a0429ec79fd01f0994bce7
HadjSassi <mahdihadjsassi1@gmail.com>
2025-02-17 23:43:07 +0100

    Achievements

--- src/app/achievements/achievements.component.ts
+++ src/app/achievements/achievements.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {CommonModule} from '@angular/common';
+
+interface Achievement {
+  title: string;
+  date: string;
+  location: string;
+  description: string;
+  link: string;
+  mediaLink: string;
+}
+
+interface AchievementCategory {
+  category: string;
+  items: Achievement[];
+}
 
 @Component({
   selector: 'app-achievements',
-  imports: [],
   templateUrl: './achievements.component.html',
-  styleUrl: './achievements.component.css'
+  styleUrls: ['./achievements.component.css'],
+  imports: [CommonModule]
 })
-export class AchievementsComponent {
+export class AchievementsComponent implements OnInit {
+  achievements: AchievementCategory[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchAchievements();
+  }
+
+  fetchAchievements() {
+    //todo fix all the rest of the achivements entries
+    this.http.get<{ achievements: AchievementCategory[] }>('data/achievements.json').subscribe(data => {
+      this.achievements = data.achievements;
+    });
+  }
 }
