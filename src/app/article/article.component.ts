214bc00c1bf22602929e01b4417ee2ce44f9ea50
HadjSassi <mahdihadjsassi1@gmail.com>
2025-02-18 13:55:00 +0100

    Article in progress

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-article',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './article.component.html',
   styleUrl: './article.component.css'
 })
 export class ArticleComponent {
-
+  @Input() article: any;
+  @Input() titleArticle: any;
 }
--- src/app/it-trainer/it-trainer.component.ts
+++ src/app/it-trainer/it-trainer.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Header2Component} from '../header2/header2.component';
 import {FooterComponent} from '../footer/footer.component';
+import {ArticleComponent} from '../article/article.component';
+import trainerData from '../../../public/data/trainer.json';
+import {CommonModule} from '@angular/common';
+
 
 @Component({
   selector: 'app-it-trainer',
   imports: [
     Header2Component,
-    FooterComponent
+    FooterComponent,
+    ArticleComponent,
+    CommonModule
   ],
   templateUrl: './it-trainer.component.html',
   styleUrl: './it-trainer.component.css'
 })
-export class ItTrainerComponent {
+export class ItTrainerComponent implements OnInit{
   profileImage: string = "HadjSassisamll.png";
   titlePage: string = "An It Trainer";
+  titleArticle: string = "Training";
+  trainings = trainerData.trainings;
+
+  ngOnInit() {
+    console.log(this.trainings);
+  }
 
 }
