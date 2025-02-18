bb611757199cea528ba7aa65cb0c24117f978406
HadjSassi <mahdihadjsassi1@gmail.com>
2025-02-18 01:39:16 +0100

    back to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,14 @@
-import { Component } from '@angular/core';
-import {NavbarComponent} from './navbar/navbar.component';
-import {RouterOutlet} from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'HADJ SASSI';
-
-  scrollToBottom() {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
-  }
-
-  scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
-
 }
-
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {HeaderComponent} from '../header/header.component';
 import {AboutComponent} from '../about/about.component';
 import {FooterComponent} from '../footer/footer.component';
@@ -10,6 +10,7 @@ import {AchievementsComponent} from '../achievements/achievements.component';
 import {SocialLifeComponent} from '../social-life/social-life.component';
 import {ReviewsComponent} from '../reviews/reviews.component';
 import {ContactComponent} from '../contact/contact.component';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -24,11 +25,29 @@ import {ContactComponent} from '../contact/contact.component';
     AchievementsComponent,
     SocialLifeComponent,
     ReviewsComponent,
-    ContactComponent
+    ContactComponent,
+    CommonModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  showBackToTopButton = false;  // Track whether the back-to-top button should be shown
+  scrollThreshold = 50;  // The threshold for scrolling
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    // Show back-to-top button if scroll position is greater than threshold
+    this.showBackToTopButton = currentScrollPosition > this.scrollThreshold;
+  }
+
+  scrollToBottom(): void {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
